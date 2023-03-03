4394a5ca46f840c0f4c08dd538ce3d0aeb1b5419
Andy Shultz <ashultz@edx.org>
2023-03-03 09:38:55 -0500

    feat: summary hook aside
    The summary hook aside attaches to units in specific courses
    (controlled by a course waffle flag) and if there is enough HTML in
    there to indicate summarizable text, brings in the AI summary
    javascript from the 2U ai-spot service.
    
    It needs "where is that service" and "how much text really" settings
    configurable by environment.

--- setup.py
+++ setup.py
@@ -110,6 +110,7 @@ setup(
     url='https://github.com/edx/edx-arch-experiments',
     packages=[
         'edx_arch_experiments',
+        'edx_arch_experiments.summaryhook_aside',
     ],
     include_package_data=True,
     install_requires=load_requirements('requirements/base.in'),
@@ -130,6 +131,10 @@ setup(
     entry_points={
         "lms.djangoapp": [
             "arch_experiments = edx_arch_experiments.apps:EdxArchExperimentsConfig",
+            "summaryhook = edx_arch_experiments.summaryhook_aside.apps:SummaryHookConfig",
+        ],
+        "xblock_asides.v1": [
+            "summaryhook_aside = edx_arch_experiments.summaryhook_aside.block:SummaryHookAside",
         ],
     },
 )
