6037ac399e24bbe1d53940278797a9b07e4c91a7
Tarun Tak <ttak-apphelix@2u.com>
2026-07-06 08:57:49 +0000

    fix: import position

--- edx_arch_experiments/certificates/views.py
+++ edx_arch_experiments/certificates/views.py
@@ -16,6 +16,7 @@ from urllib.parse import urlparse
 import backoff
 import boto3
 from botocore.exceptions import ClientError
+from django.db.models import Q
 from lms.djangoapps.certificates.data import CertificateStatuses  # pylint: disable=import-error
 from lms.djangoapps.certificates.models import GeneratedCertificate  # pylint: disable=import-error
 from openedx.core.djangoapps.user_api.accounts.permissions import CanRetireUser  # pylint: disable=import-error
@@ -23,7 +24,6 @@ from rest_framework import permissions, status
 from rest_framework.parsers import JSONParser
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from django.db.models import Q
 
 log = logging.getLogger(__name__)
 
